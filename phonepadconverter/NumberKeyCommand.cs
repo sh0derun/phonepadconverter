3754b1f8988fccfce09ad5a5789e6e466cd3adb2
Sh@drun <5242001+sh0derun@users.noreply.github.com>
2023-04-19 16:54:01 +0700

    implementing the challenge in command pattern

---
+++ phonepadconverter/NumberKeyCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace phonepadconverter
+{
+    class NumberKeyCommand : KeyCommand
+    {
+        private char number;
+
+        public NumberKeyCommand(PhonePadConverter converter, char number): base(converter)
+        {
+            this.number = number;
+        }
+
+        public override void Execute()
+        {
+            converter.PressButton(number);
+        }
+    }
+}
--- phonepadconverter/Program.cs
+++ phonepadconverter/Program.cs
@@ -6,75 +6,43 @@ namespace phonepadconverter
     public class Program
     {
 
-        private static readonly Dictionary<char, string> buttonMap = new Dictionary<char, string>{
-                { '0', " " },
-                { '2', "ABC" },
-                { '3', "DEF" },
-                { '4', "GHI" },
-                { '5', "JKL" },
-                { '6', "MNO" },
-                { '7', "PQRS" },
-                { '8', "TUV" },
-                { '9', "WXYZ" }
-            };
+        public PhonePadConverter converter { get; private set; }
+
+        public Program()
+        {
+            converter = new PhonePadConverter();
+        }
 
         static void Main()
         {
-            Console.Write("Enter the input numbers from the old phone pad :");
-            string input = Console.ReadLine();
-            Console.WriteLine(Program.OldPhonePad(input));//"7777255562*2207777255562*22*2#"));
+            Console.WriteLine(new Program().OldPhonePad("8 88777444666*664#"));
             Console.Read();
         }
 
-        public static string OldPhonePad(string input)
+        public string OldPhonePad(string input)
         {
-
-            int offset = 0;
-            string acc = "";
-            char current = '\0';
+            KeyCommand command = null;
 
             foreach (char c in input)
             {
                 if (Char.IsDigit(c))
                 {
-                    if (current == '\0')
-                    {
-                        current = c;
-                        offset++;
-                    }
-                    else
-                    {
-                        if (current == c)
-                        {
-                            offset++;
-                            current = c;
-                        }
-                        else
-                        {
-                            string chars = buttonMap[current];
-                            acc += chars[(offset + (offset > 0 ? -1 : 0)) % chars.Length];
-                            current = c;
-                            offset = 1;
-                        }
-                    }
+                    command = new NumberKeyCommand(converter, c);
                 }
                 else if (c == '#' || c == ' ')
                 {
-                    if (current != '\0')
-                    {
-                        string chars = buttonMap[current];
-                        acc += chars[(offset + (offset > 0 ? -1 : 0)) % chars.Length];
-                    }
-                    offset = 0;
+                    command = new SendKeyCommand(converter);
                 }
                 else if (c == '*')
                 {
-                    current = '\0';
-                    offset = 0;
+                    command = new BackspaceKeyCommand(converter);
+                }
+                if(command != null)
+                {
+                    command.Execute();
                 }
             }
-
-            return acc;
+            return converter.accumulator;
         }
     }
 }
